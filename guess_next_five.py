04ee70677f2a3067185912d0ba8193421601cc6b
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-12 16:18:46 +0000

    dummy: pushing the script without testing, fastai, yet to finish installing

---
+++ guess_next_five.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""guess next five"""
+
+from fastai.collab import *
+from fastai.tabular.all import *
+
+
+if __name__ == "__main__":
+    learn_inf = load_learner('./export.pkl')
+    dls_inf = learn_inf.dls
+    joke_factors_inf = learn_inf.model.item_factors.weight
+    idx_int = 19
+    cls_idx = tensor(dls_inf.classes['jokeId'].o2i[idx_int])
+    int_joke_emb = joke_factors_inf[cls_idx, None]
+    distances = nn.CosineSimilarity(dim=1)(joke_factors_inf, int_joke_emb)
+    closest_emb_idx = distances.argsort(descending=True)[1:6] #Top 5 closest jokes.
+    closest_idx = dls_inf.classes['jokeId'][closest_emb_idx]
+    # Print the actual joke.
+    print('The actual joke is:')
+    print(ratings_df[ratings_df['jokeId'] == idx_int].head(1).jokeText)
+    print('========================================')
+    closest_jokes = ratings_df[ratings_df["jokeId"].isin(closest_idx)].drop_duplicates("jokeId")["jokeText"]
+    # closest_jokes = closest_jokes.str.replace("\n", " ")
+    print('The closest jokes are:')
+    for joke in closest_jokes:
+        print(joke, "\n\n")
