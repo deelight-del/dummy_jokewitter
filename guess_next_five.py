aea8c740ace39eed5e5ed68a28c0e4d009a85b16
deelight-del <akingbenidavidoyin@gmail.com>
2024-06-13 18:40:52 +0000

    Now working

--- guess_next_five.py
+++ guess_next_five.py
@@ -5,11 +5,34 @@
 from fastai.collab import *
 from fastai.tabular.all import *
 
+
+#We need a model to train the data on,
+class DotProduct(Module):
+    def __init__(self, n_users, n_items, n_factors, y_range=(-9.5, 10.5)):
+        self.user_factors = Embedding(n_users, n_factors)
+        self.user_bias = Embedding(n_users, 1)
+        self.item_factors = Embedding(n_items, n_factors)
+        self.item_bias = Embedding(n_items, 1)
+        self.y_range = y_range
+        pass
+    
+    def forward(self, x):
+        users = self.user_factors(x[:,0])
+        items = self.item_factors(x[:,1])
+        res = (users * items).sum(dim=1, keepdim=True)
+        res += self.user_bias(x[:,0]) + self.item_bias(x[:,1])
+        return sigmoid_range(res, *self.y_range)
+
+
+
 # Load our dataframe.
 ratings_df = pd.read_pickle('./mini_ratings-df.pkl')
 
+# Load our learner.
+learn_inf = load_learner('./export-3-10.pkl')
+
+
 if __name__ == "__main__":
-    learn_inf = load_learner('./export.pkl')
     dls_inf = learn_inf.dls
     joke_factors_inf = learn_inf.model.item_factors.weight
     idx_int = 19
---
+++ version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from fastai import __version__
+from fastai.collab import *
+from fastai.tabular.all import *
+
+print("version of pytorch", torch.__version__)
+
+print(__version__)
